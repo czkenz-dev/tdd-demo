76fd053e74b0fe69500894d4e5494f80f99a5aa5
Kenz <yutthagarn.ken@gmail.com>
2020-09-26 12:57:33 +0700

    add new unit test

--- src/test/java/com/example/demo/CalculatorTest.java
+++ src/test/java/com/example/demo/CalculatorTest.java
@@ -1,26 +1,34 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.Test;
-
 public class CalculatorTest {
 
-	@Test
-	void calculateValid() {
-		int num1 = 3;
-		int num2 = 4;
-		int expectResult = num1 * num2;
-		assertEquals(12, expectResult);
-	}
+    @Test
+    void calculateValid() {
+        int num1 = 3;
+        int num2 = 4;
+        int expectResult = num1 * num2;
+        assertEquals(12, expectResult);
+    }
 
-	@Test
-	void calculateInvalid() {
-		int num1 = 3;
-		int num2 = 4;
-		int expectResult = num1 * num2;
-		assertNotEquals(15, expectResult);
-	}
+    @Test
+    void calculateValid2() {
+        int num1 = 5;
+        int num2 = 4;
+        int expectResult = num1 * num2;
+        assertEquals(20, expectResult);
+    }
+
+    @Test
+    void calculateInvalid() {
+        int num1 = 3;
+        int num2 = 4;
+        int expectResult = num1 * num2;
+        assertNotEquals(15, expectResult);
+    }
 
 }
